1e059e492dc1777904c02092d5c4a97a895bdb16
luis <foreverluiscastro@gmail.com>
2021-07-25 08:58:02 -0400

    added index create actions and private params to appointments.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,37 @@
 class AppointmentsController < ApplicationController
+    # GET /appointments
+    def index
+        if session[:client_id]
+            client = Client.find(session[:client_id])
+            appointments = client.appointments
+            render json: appointments, include: :walker
+        elsif session[:walker_id]
+            walker = Walker.find(session[:walker_id])
+            appointments = walker.appointments
+            render json: appointments, include: :client
+        else
+            render json: { error: [ "Not Authorized" ] }, status: :unauthorized
+        end
+    end
+
+    # POST /appointments
+    def create
+        if session[:client_id]
+            client = Client.find(session[:client_id])
+            appointment = client.appointments.create(appointment_params)
+            if appointment.valid?
+                render json: appointment, include: :walker, status: :created
+            else
+                render json: { error: [ "Invalid input. Check the information and try again." ] }, status: :unprocessable_entity
+            end
+        else
+            render json: { error: [ "Not Authorized" ] }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def appointment_params
+        params.permit(:walker_id, :client_id)
+    end
 end
