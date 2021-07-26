4314146e33f9bd15a53079fa03ff0755f76cdf5d
luis <foreverluiscastro@gmail.com>
2021-07-26 12:18:04 -0400

    Added a create and destroy action to post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,42 @@
 class PostsController < ApplicationController
+    # POST /job-postings
+    def create
+        if session[:client_id]
+            client = Client.find(session[:client_id])
+            post = client.posts.create(post_params)
+            if post.valid?
+                render json: post, include: :client, status: :created
+            else
+                render json: { error: [ "Invalid input. Check the information and try again." ] }, status: :unprocessable_entity
+            end
+        else
+            render json: { error: [ "Not Authorized" ] }, status: :unauthorized
+        end
+    end
+
+    # GET /job-postings
+    def index
+
+    end
+
+    # DELETE /job-postings/:id
+    def destroy
+        if session[:walker_id]
+            post = Post.find_by(id: params[:id])
+            if post
+                post.delete
+                head :no_content
+            else
+                render json: { error: [ "Could not complete request. Please Try again" ] }, status: :unprocessable_entity
+            end
+        else
+            render json: { error: [ "Not Authorized" ] }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def post_params
+        params.permit(:client_id, :appointment_date)
+    end
 end
